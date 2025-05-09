cb953038accc339b636bb12d02b02b35b9a871a8
Suprith S <supriths4804@gmail.com>
2025-05-09 19:28:50 +0530

    Update

--- App/src/App.js
+++ App/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
-import Dashboard from './Dashboard';
-import CreateEvent from './createevent';
-import ViewEvent from './view_event';
 import LoginPage from './LoginPage';
 import AdminCreateUserPage from './admin-dashboard';
 import OfficerDashboard from './officer-dashboard';
@@ -20,9 +17,6 @@ const App = () => {
         <Route path="/admin-dashboard" element={<AdminCreateUserPage />} />
         <Route path="/officer-dashboard" element={<OfficerDashboard />} />
         <Route path="/examiner-dashboard" element={<ExaminerDashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create-event" element={<CreateEvent />} />
-        <Route path="/view-event/:event_id" element={<ViewEvent />} />
         <Route path="/logs" element={<Logs />} />
         <Route path="/head-investigator-dashboard" element={<InvestigatorDashboard />} />
       </Routes>
